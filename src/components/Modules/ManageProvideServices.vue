443f791cd75c2d378c497b54500548b9c1cc5670
LUHMLLO <luis.jr.melo@gmail.com>
2019-03-24 19:41:45 -0400

    can create and read services

--- src/components/Modules/ManageProvideServices.vue
+++ src/components/Modules/ManageProvideServices.vue
@@ -11,7 +11,7 @@
                     
                     <div class="global-service-card mdl-shadow--3dp"   v-for="(service,servicesData) in services" :key="servicesData">
                         <div class="global-service-card-img">
-                            <img src="https://cdn.dribbble.com/users/63407/screenshots/5462636/dribbble_taxi_cab_ride.png">
+                            <img v-bind:src="service.img">
                         </div><!-- global service card img-->
                         <div class="global-service-card-content">
                             <div class="global-service-card-name">
@@ -19,6 +19,7 @@
                             </div>
                             <div class="global-service-card-description">
                                 <p>{{service.description}}</p>
+                                <small>{{service.price}}</small>
                             </div>
                         </div><!---global service card content -->
                     </div><!--service card-->
@@ -26,7 +27,7 @@
             </div><!---available section-->
 
 
-            <br><br><br><br>
+            
 
 
             <div id="unavailable-section" class="global-section">
@@ -35,20 +36,54 @@
                     
                     <div class="global-service-card mdl-shadow--3dp"   v-for="(service,servicesData) in services" :key="servicesData">
                         <div class="global-service-card-img">
-                            <img src="https://cdn.dribbble.com/users/63407/screenshots/5462636/dribbble_taxi_cab_ride.png">
-                        </div><!-- global service card img-->
+                            <img v-bind:src="service.img">
+                         </div><!-- global service card img-->
                         <div class="global-service-card-content">
                             <div class="global-service-card-name">
                                 <h4>{{service.name}}</h4>
                             </div>
                             <div class="global-service-card-description">
                                 <p>{{service.description}}</p>
+                                <small>{{service.price}}</small>
                             </div>
                         </div><!---global service card content -->
                     </div><!--service card-->
                 </div>
             </div><!---available section-->
 
+
+
+            <div id="unavailable-section" class="global-section">
+              <h4>Add a new service</h4>
+              
+              <form @submit.prevent>
+                  <div class="global-grid">
+                        <div class="global-floating-input">
+                            <i class='uil uil-file-blank'></i>
+                            <input type="text" name="" id="" v-model="newserviceimg" placeholder="img (url only)" required>
+                        </div><!--global floating input-->
+        
+                        <div class="global-floating-input">
+                            <i class='uil uil-file-blank'></i>
+                            <input type="text" name="" id="" v-model="newservicename" placeholder="name" required>
+                        </div><!--global floating input-->                
+
+                        <div class="global-floating-input">
+                            <i class='uil uil-file-blank'></i>
+                            <input type="text" name="" id="" v-model="newservicedescription" placeholder="description" required>
+                        </div><!--global floating input-->
+        
+                        <div class="global-floating-input">
+                            <i class='uil uil-file-blank'></i>
+                            <input type="text" name="" id="" v-model="newserviceprice" placeholder="price" required>
+                        </div><!--global floating input-->              
+                  </div><!--global grid-->
+                  
+                  <br><br>
+                  <button class="global-button" v-on:click="addService">Add new service</button>
+
+              </form>
+            </div><!---available section-->
 
 
 
@@ -65,18 +100,55 @@
 
 
 <script>
+import {db} from '../../firebaseConfig.js'
+
 export default {
     name: 'ManageProvideServices',
     data(){
         return{
 
-            services:[
-                {name:'service name', description: 'service description goes here'},
-                {name:'service name', description: 'service description goes here'},
-                {name:'service name', description: 'service description goes here'},
-            ],
+            newserviceimg:null,
+            newservicename:null,
+            newservicedescription:null,
+            newserviceprice:null,
+
+            services:[],
 
         }
+    },
+
+    methods:{
+
+        addService(){
+            db.collection('services').add({
+                img: this.newserviceimg,
+                name: this.newservicename,
+                description: this.newservicedescription,
+                price: this.newserviceprice,
+            })
+            .catch(error => alert(error))
+        }
+
+    },
+
+    created(){
+        let self = this;  
+
+          db.collection('services').orderBy('name').get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) =>{
+              //console.log(doc.data().name) 
+                 const data ={
+                   'img': doc.data().img,
+                   'name': doc.data().name,
+                   'description': doc.data().description,
+                   'price': doc.data().price
+                 }
+                   self.services.push(data)
+  
+            })
+          });
     }
+
+
 }
 </script>
