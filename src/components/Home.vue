32ef315d23fa750d4b0778097328ccffcfc024e8
LUHMLLO <luis.jr.melo@gmail.com>
2019-03-30 23:08:10 -0400

    working on the dashboard

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,9 @@
 <template>
-<div>
+<div id="home">
 
 
   <div class="global-horizontal-scroll">
+    <h3>Latest reviews</h3>
     <div class="global-card-small mdl-shadow--2dp global-horizontal-position" v-for="(topCard,topCardsData) in topCards" :key="topCardsData">
       <i class='uil uil-chart-pie'></i>
       <p>{{topCard.something}}</p>
@@ -14,34 +15,56 @@
 
 
 
-   <div id="dashboard" class="global-container mdl-shadow--2dp">
+   <div id="dashboard" class="global-grid">
+
+
       
-          <div class="global-col">
-            <h3>this module title here</h3>
-            <div class="global-container">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quod maxime sapiente perferendis repellat officia, aliquid quos quibusdam blanditiis sequi voluptates voluptatum dolore quam, repudiandae iusto ipsa suscipit, labore dignissimos!</p>
+          <div id="stadistics" class="global-col">
+            <div class="home-dashboard-panels-headers">
+              <h3>Stadistics</h3><button class="global-button">action</button>
             </div>
-          </div>
+          </div><!---------->
 
 
-      <div class="global-grid">
-          
-            <div id="yourservices" class="global-col">
-              <h3>this module title here</h3>
-              <div class="global-container">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora vero ratione odio, quos dolore aliquid! Doloremque porro, dolorum minima recusandae quia officiis, aliquid, iusto corporis iste ad reprehenderit exercitationem non!</p>
-              </div>
+          <div id="activeServices" class="global-col">
+            
+            <div class="home-dashboard-panels-headers">
+              <h3>Services</h3><button class="global-button" v-on:click="addNewServiceFromHome">new service</button>
             </div>
-          <!------------>
-            <div id="somethingidk" class="global-col">
-              <h3>this module title here</h3>
-              <div class="global-container">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi illo odio asperiores quia? Cupiditate, necessitatibus velit nulla quibusdam ducimus, quas error odit numquam, excepturi tempora aliquid facere cumque. Alias, unde.</p>
-              </div>
+
+            <div id="viewActiveServicesHome" class="global-container global-grid" v-if="!addingNewService">
+
+                 <div class="global-item-preview-rectangle mdl-shadow--2dp" v-for="(service,activeServicesData) in activeServices" :key="activeServicesData">
+                   <div class="global-item-preview-rectangle-img mdl-shadow--2dp">
+                     <img v-bind:src="service.img">
+                   </div>
+                   <div class="global-item-preview-rectangle-content">
+                     <h3>{{service.name}}</h3>
+                     <p>{{service.description}}</p>
+                   </div>
+                  
+                  <router-link :to="{ name: 'ManageServices_Edit', params: {service_id: service.service_id}}">
+                    <i class='uil uil-pen'></i>
+                  </router-link>                
+                 </div><!---->
+
             </div>
+            <div id="addNewServicesHome" class="global-container" v-if="addingNewService">
+              <h3>here its gonna be the crud</h3>
+              <button class="global-button" v-on:click="addNewServiceFromHomeFinish">okay</button>
+            </div>
+          </div><!---------->
+
+
+          <div id="somethingidk" class="global-col">
+            <div class="home-dashboard-panels-headers">
+              <h3>module title here</h3><button class="global-button">action</button>
+            </div>
+          </div><!---------->
+
+
 
       </div><!---global grid-->
-   </div><!---global conteiner--->
 
 
 
@@ -53,6 +76,8 @@
 </template>
 
 <script>
+import {firebase, db} from '../firebaseConfig.js'
+
 export default {
   name: "Home",
   data() {
@@ -64,30 +89,98 @@ export default {
         {'something': 'something in here idk'},
         {'something': 'something in here idk'},       
       ],
-    
+
+      activeServices:[],
+
+
+      addingNewService:false,
     
     
     }
-  }
+  },
+
+
+
+
+
+
+
+  created(){
+        let self = this;
+
+          db.collection('services').where('owner_id','==',firebase.auth().currentUser.uid).get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                //console.log(doc.data().schedule)
+                
+                const data ={
+                  
+                   'service_id': doc.data().service_id,
+                   'owner_id': doc.data().owner_id,
+                   'img': doc.data().img,
+                   'name': doc.data().name,
+                   'description': doc.data().description,
+                   'price': doc.data().price,
+                   'schedule': doc.data().schedule,
+                   'availability': doc.data().availability,
+                   'url_name': doc.data().url_name,
+                 }
+
+                 if(doc.data().availability == true){
+                   self.activeServices.push(data)
+                 }
+
+            })  
+        })
+    },
+
+
+    methods:{
+      addNewServiceFromHome:function(){
+        this.addingNewService = true
+      },
+      addNewServiceFromHomeFinish:function(){
+        this.addingNewService = false
+      }
+    }
+
+
+
+
+
+
+
+
+
 } 
 </script>
 
 <style scoped>
 #dashboard{
-  text-align: left;
-  width:90%;
-  border-radius:8px;
+  width: 96%;
+  margin: auto;
 }
-
-#somethingidk,#yourservices{
+.home-dashboard-panels-headers{
+  padding:22px 22px 52px 22px;
+  border-bottom: solid 0.5px rgba(0,0,0,0.3);
+  width:97%;
+  margin: auto auto 12px auto;
+}
+.home-dashboard-panels-headers h3{
+  font-size:26px !important;
+  float: left;
+}
+.home-dashboard-panels-headers button{
+  float: right;
+}
+#stadistics{
+  width:100%;
+  margin: 12px auto;
+}
+#activeServices,#somethingidk{
   width:50%;
+  min-width:300px;
+  margin: 12px auto;
 }
 
-.global-col h3{
-  padding:22px;
-}
-.global-col{
-  padding:22px;
-}
 
 </style>
