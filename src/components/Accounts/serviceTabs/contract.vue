ab83c0dcc78e238a3afd9fa1e55a9a0783c0a5dc
erquier <ernest324@gmail.com>
2019-04-02 14:02:37 -0400

    deleting a few components

--- src/components/Accounts/serviceTabs/contract.vue
+++ src/components/Accounts/serviceTabs/contract.vue
@@ -1,55 +1,10 @@
 <template>
-<div class="container" :class="{loadingItem: isProductLoading}">
-  <div class="row text-center" v-if="isProductLoading">
-    <grid-loader :loading="isProductLoading" :color="loaderColor" :size="loaderSize"></grid-loader>
-  </div>
-  <div v-else class="row action-panel">
-    <div class="col-xs-12">
-      <div class="btn-group pull-right">
-				<a id="list" class="btn btn-default btn-sm" @click.prevent="changeDisplay(true)">
-					<span class="glyphicon glyphicon-th-list"></span>List
-				</a>
-				<a id="grid" class="btn btn-default btn-sm" @click.prevent="changeDisplay(false)">
-					<span class="glyphicon glyphicon-th"></span>Grid
-				</a>
-      </div>
-    </div>
-  </div>
 
-  <div class="is-flex" v-if="!isProductLoading">
-    <app-product-item v-for="prod in products" :item="prod" :key="prod.id" :displayList="displayList"></app-product-item>
-  </div>
-
-</div>
 </template>
 
 
 
 <script>
-import { mapGetters } from 'vuex'
-// import ProductItem from '../../components/Modules/ShoppingCart/ProductItem.vue';
-import GridLoader from 'vue-spinner/src/GridLoader.vue';
 
-export default {
-  data() {
-    return {
-      loaderColor: "#5cb85c",
-      loaderSize: "50px",
-      displayList: false
-    }
-  },
-  computed: {
-    ...mapGetters(['products', 'isProductLoading']),
-  },
-  components: {
-    
-    GridLoader
-  },
-  methods: {
-    changeDisplay(isList) {
-      this.displayList = isList;
-    }
-  }
-}
 </script>
 
