11da89f419dbd26caadbbe66d256cbe0224bc376
erquier <ernest324@gmail.com>
2019-04-02 14:06:54 -0400

    deleting another import

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import firebase from './firebaseConfig.js'
-import store from './stores/store';
-
 
 
 Vue.config.productionTip = false
@@ -17,8 +15,7 @@ let app;
 firebase.auth().onAuthStateChanged(() => {
   if(!app){
     app = new Vue({
-      router,
-      store,
+      router,      
       firebase,
       render: h => h(App),
     }).$mount('#app');
